b7b83e62d74ec5b42a6e1a92bd2b760ba7a86f4e
Doney Peters <doneypeters@gmail.com>
2021-03-11 23:51:03 -0800

    removed 'secret' key that will not be used

--- CodenameStonks/settings.py
+++ CodenameStonks/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8ou^hz9$%#ua&-*!+v2+4xhb3bf!p#as+ijl04qvl+h0py0+=3'
+SECRET_KEY = 'test'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
