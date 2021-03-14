9d07b48f58ae65ba6797a0a83b25bb36206f25a1
Doney Peters <doneypeters@gmail.com>
2021-03-14 09:55:25 -0700

    Added Stock History Endpoint

--- InvestmentTracker/urls.py
+++ InvestmentTracker/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<coin>/', views.getCryptoPrice, name='getCryptoPrice'),
+    path('GetCryptoPrice/<coin>', views.getCryptoPrice, name='getCryptoPrice'),
+    path('GetStockPriceHistory/<stock>/<interval>', views.getStockPriceHistory, name='getStockPriceHistory')
 ]
--- InvestmentTracker/views.py
+++ InvestmentTracker/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import requests, json
+import requests
+import json
+
+# insert key before testing
+apikey = '<Insert Private Key>'
 
 # Create your views here.
 def getCryptoPrice(request, coin):
 
     data = {
-        'apikey': '<insertkey>',
+        'apikey': f'{apikey}',
         'function': 'CURRENCY_EXCHANGE_RATE',
         'from_currency': f'{coin}',
         'to_currency': 'USD'
@@ -18,3 +22,22 @@ def getCryptoPrice(request, coin):
         "json_pretty": json_pretty,
     }
     return render(request, "CryptoOutput.html", context)
+
+def getStockPriceHistory(request, stock, interval):
+
+
+    data = {
+        'apikey': f'{apikey}',
+        'function': 'TIME_SERIES_INTRADAY',
+        'symbol': f'{stock}',
+        'interval': f'{interval}',
+        'outputsize': 'full'
+    }
+    r = requests.get("https://www.alphavantage.co/query", params=data)
+    json_data = r.json()
+    json_pretty = json.dumps(json_data, sort_keys=True, indent=4)
+    context = {
+        "json_pretty": json_pretty,
+    }
+    return render(request, "CryptoOutput.html", context)
+
