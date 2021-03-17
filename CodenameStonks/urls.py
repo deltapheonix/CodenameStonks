21a45fb3f2933220b637bbb2d9c964c032ad7978
Doney Peters <doneypeters@gmail.com>
2021-03-16 23:36:44 -0700

    Updated Urls for stock project to use a more general term.

--- CodenameStonks/urls.py
+++ CodenameStonks/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('crypto/', include('InvestmentTracker.urls'))
+    path('invest/', include('InvestmentTracker.urls'))
 ]
