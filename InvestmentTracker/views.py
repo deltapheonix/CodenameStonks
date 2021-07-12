7e121bd87c7158a00591e647180de5d123af66c7
Doney Peters <doneypeters@gmail.com>
2021-07-11 20:43:44 -0700

    Removed views.py  from .gitignore and added it back to the repo

---
+++ InvestmentTracker/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import JsonResponse
+import requests
+import json
+
+# insert key before testing
+apikey = '<Insert Private Key>'
+
+# Create your views here.
+def getCryptoPrice(request, coin):
+
+    data = {
+        'apikey': f'{apikey}',
+        'function': 'CURRENCY_EXCHANGE_RATE',
+        'from_currency': f'{coin}',
+        'to_currency': 'USD'
+    }
+    r = requests.get("https://www.alphavantage.co/query", params=data)
+    json_data = r.json()
+    #return render(request, "CryptoOutput.html", context)
+    return JsonResponse(json_data)
+
+def getStockPriceHistory(request, stock, interval):
+
+
+    data = {
+        'apikey': f'{apikey}',
+        'function': 'TIME_SERIES_INTRADAY',
+        'symbol': f'{stock}',
+        'interval': f'{interval}',
+        'outputsize': 'full'
+    }
+    r = requests.get("https://www.alphavantage.co/query", params=data)
+    json_data = r.json()
+    # json_pretty = json.dumps(json_data, sort_keys=True, indent=4)
+    # context = {
+    #     "json_pretty": json_pretty,
+    # }
+    #return render(request, "CryptoOutput.html", context)
+    return JsonResponse(json_data)
+
